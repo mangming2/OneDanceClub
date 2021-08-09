1729fc765b53f062ac265afe43d976f62ceb6eaa
mangming2 <59263564+mangming2@users.noreply.github.com>
2021-08-09 23:53:12 +0900

    Aboutus_jquery
    aboutus js 구현 완료

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -17,51 +17,56 @@ const genrelist={
     "genre_name": "힙합",
     "explain":"1990년대 초부터 미국 청소년들 사이에서 유행한 자유스럽고 즉흥적인 춤입니다. 디스코댄스에 기반을 둔 춤으로서 일정한 음이 계속되는 힙합음악에 맞추어 춤을 춘 것이 힙합댄스의 시초이며, 올드 스쿨 힙합은 그루브와 박자를 큰 바운스로 타주는게 특징입니다.", 
     "img": "./image/aboutus/hiphop.jpg",
-    "link":"https://youtu.be/UYgfSVhKSOM"
+    "link":"https://youtu.be/F7YmE91ilc4"
   },
 
   "wacking":{
     "genre_name": "왁킹",
     "explain":" 기본동작은 주로 팔을 돌리는 동작인 트월(Twirl)과 팔을 뻗는 동작인 익스텐션(Extension), 모델처럼 인상적인 포즈를 취하는 포징(Posing)과 약간의 스텝(Step)으로 구성되며, 춤을 추는 태도(Attitude)가 일반적으로 중시되는 춤입니다. 특히 왁킹에서 중시되는 것은 그 태도로, 왁킹은 모티브가 당시 영화와 드렉퀸 쇼에서 나왔기 때문에 음악의 무드를 느끼며 섹슈얼하고 연기적인 부분이 강조됩니다.",
     "img": "./image/aboutus/wacking.jpg",
-    "link":"https://youtu.be/5pbjQZrMrGQ"
+    "link":"https://youtu.be/tCDqCnOqOas"
     },
 
   "locking":{
     "genre_name": "락킹", 
     "explain":"클럽에서 춤을 추던 돈 캠벨이 동작을 삐끗하는 바람에 마치 자물쇠가 잠기듯(Lock) 몸이 툭하고 멈추었던 것이 주변 사람들에게 큰 호응을 이끌어낸 사건에서 유래한 춤입니다. 이후 돈 캠벨이 최초의 락킹 댄스팀인 더 락커스(The Lockers)를 조직해 활동함으로써 세계적으로 유명해졌습니다. 다른 춤들과 구분되는 특징적인 요소는 바로 익살스러운 캐릭터(Character)이며, 대놓고 방정맞은 태도를 취하기도, 능청스러운 분위기를 연출하기도, 치기어리고 귀여운 모습을 연출하기도 합니다.", 
     "img": "./image/aboutus/locking.jpg",
-    "link":"https://youtu.be/zyMfpJh3h4A"},
+    "link":"https://youtu.be/EkgihFiKcME"},
 
   "poppin":{
     "genre_name": "팝핀", 
     "explain":"1975년 미국 캘리포니아주 프레즈노 출신의 부갈루 샘이 창안한 장르입니다. 몸 각 부위 근육에 빠르게 힘을 주었다가 이완시키는 팝(POP) 기술을 토대로 이루어지기 때문에 이러한 이름이 붙었습니다. 팝핀은 기본적으로 음악의 베이스, 또는 드럼과 같은 소리에 맞추어 몸이 터지는 듯한 느낌으로 음악을 표현합니다. 또한 팝핀이 다른 여러 가지 느낌의 스타일과 접목되어 발전함에 따라 박과 박 사이의 음, 또는 추가적인 음악 요소를 표현하는 방법도 크게 이용되고 있습니다.",
     "img": "./image/aboutus/poppin.jpg",
-    "link":"https://youtu.be/DqDzCIfBJ5M"},
+    "link":"https://youtu.be/5pbjQZrMrGQ"},
 
   "krump":{
     "genre_name": "크럼프",
     "explain":" 에너지를 가지고노는 춤으로서 자유롭고 격렬하며 역동적인 움직임이 특징인 춤입니다. 가슴을 튕기는 체스트 팝(Chest Pop), 팔을 휘두르는 암 스윙(Arm Swing), 발을 구르는 스텀프(Stomp) 등의 기술로 이루어지며 그 외에 부가적인 요소로서 모자와 신발, 옷 등 도구를 이용하는 트릭을 선보이기도 합니다.",
     "img": "./image/aboutus/krump.jpg",
-    "link":"https://youtu.be/9P72bkNKfow"},
+    "link":"https://youtu.be/XbuyE2pSvN4"},
 
   "house":{
     "genre_name": "하우스",
     "explain":"하우스 음악에 맞춰 추는 클럽용 댄스가 시초이며, 빠른 스텝과 그루브에서 나오는 풍부한 박자감, 음악의 특성상 빠른 진행을 보이는 템포, 변화무쌍하게 음악의 리듬을 풋웍으로 표현하는 춤입니다.", 
     "img": "./image/aboutus/house.jpg",
-    "link":"https://youtu.be/o9tTUDV2tus"},
+    "link":"https://youtu.be/SubUAMdHuqA"},
 
   "bboy":{
     "genre_name": "비보이", 
     "explain":"70년대 초 브레이킹 비트가 사용되기 시작하면서 생긴 춤입니다. 스트릿 댄스 중 유일하게 플로어 중심의 무빙을 하며 스트릿 댄스 배틀문화를 주도하는 장르입니다. 스타일무브, 파워 무브, 프리즈 무브 등으로 동작이 나뉘며 화려함과 박력을 가지고 있는 춤입니다.", 
     "img": "./image/aboutus/bboy.jpg",
-    "link":"https://youtu.be/SubUAMdHuqA"},
+    "link":"https://youtu.be/d5NMdOrR1e4"},
   
   
 
 }
 
-
+function init() {
+  $('#genre_img').attr('src',genrelist["choreo"]["img"])
+}
+init();
 $('.btn').click( function() {
+    $('#genre_img').attr('src',genrelist[$(this).attr("id")]["img"])
     $('#explain').html(genrelist[$(this).attr("id")]["explain"])
+    $('#genre_link').attr('href',genrelist[$(this).attr("id")]["link"])
   });
