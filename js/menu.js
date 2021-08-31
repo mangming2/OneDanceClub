73e537097886cd6e56458d35ec541b2a3c63071c
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-08-31 15:06:17 +0900

    Newmenu (#11)
    * menu
    
    * update menu
    
    1. 햄버거 버튼 누르면 메뉴 기능나오게 추가!
    
    Co-authored-by: mangming2 <59263564+mangming2@users.noreply.github.com>

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+function sideMenu() {
+    let menu = document.querySelector(".menu");
+    let side_menu = document.querySelector(".side_menu");
+    menu.classList.toggle("active");
+    side_menu.classList.toggle("active");
+}
